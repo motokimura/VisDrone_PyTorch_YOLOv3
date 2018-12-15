e1ddc41f4e12e8fab508968cfb2e8e590975309c
motokimura <motokimura1@gmail.com>
2018-12-15 11:24:11 +0900

    Add some util shell scripts for Docker

---
+++ docker/exec.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Exec
+CONTAINER="yolov3-visdrone"
+
+nvidia-docker exec -it ${CONTAINER} /bin/bash
---
+++ docker/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set image name
+IMAGE="yolov3-visdrone:latest"
+if [ $# -eq 1 ]; then
+    IMAGE=$1
+fi
+
+# Set project root dicrectory to map to docker 
+THIS_DIR=$(cd $(dirname $0); pwd)
+PROJ_DIR=`dirname ${THIS_DIR}`
+
+# Run container
+CONTAINER="yolov3-visdrone"
+
+nvidia-docker run -it --rm \
+	-p 8888:8888 -p 6006:6006 \
+	-v ${PROJ_DIR}:/work \
+	--name ${CONTAINER} \
+	${IMAGE} /bin/bash
